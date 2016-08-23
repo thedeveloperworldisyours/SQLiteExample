2ee70d2be861ba6168e8755452b9aba393fe4531
javierG <javierg@squa0095.squay.com>
2016-08-23 17:16:56 +0100

    modified one value

--- app/src/main/java/com/thedeveloperworldisyours/sqlite/MainActivity.java
+++ app/src/main/java/com/thedeveloperworldisyours/sqlite/MainActivity.java
@@ -1,20 +1,16 @@
 package com.thedeveloperworldisyours.sqlite;
 
-import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private RateDataSource datasource;
+    private RateDataSource mDatasource;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -25,10 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        datasource = new RateDataSource(this);
-        datasource.open();
+        mDatasource = new RateDataSource(this);
+        mDatasource.open();
 
-        mValues = datasource.getAllRates();
+        mValues = mDatasource.getAllRates();
 
         mRecyclerView = (RecyclerView) findViewById(R.id.activity_main_recycler_view);
         mRecyclerView.setHasFixedSize(true);
@@ -52,13 +48,13 @@ public class MainActivity extends AppCompatActivity {
                 int nextInt = new Random().nextInt(3);
                 Random randomno = new Random();
                 // save the new Rate to the database
-                Rate = datasource.createRate(rates[nextInt], randomno.nextDouble());
+                Rate = mDatasource.createRate(rates[nextInt], randomno.nextDouble());
                 mValues.add(Rate);
                 break;
             case R.id.delete:
                 if (mValues.size() > 0) {
                     Rate = (Rate) mValues.get(0);
-                    datasource.deleteRate(Rate);
+                    mDatasource.deleteRate(Rate);
                     mValues.remove(Rate);
                 }
                 break;
@@ -68,13 +64,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-        datasource.open();
+        mDatasource.open();
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        datasource.close();
+        mDatasource.close();
         super.onPause();
     }
 
